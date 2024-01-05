ab762f313a46ae8d655026a89888b6358f51be96
Ishaan Masil <darkknightishaan@gmail.com>
2024-01-04 22:06:59 -0600

    minor changes

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "CubeBot24";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 5;
-  public static final String GIT_SHA = "0ab4c6cbeea08a09bbd3de2cd2b4ae8b4970a994";
-  public static final String GIT_DATE = "2023-12-20 13:04:17 EST";
+  public static final int GIT_REVISION = 6;
+  public static final String GIT_SHA = "174b79681a595a0decd2c9a20339fc1f8421a370";
+  public static final String GIT_DATE = "2023-12-30 20:06:12 EST";
   public static final String GIT_BRANCH = "main";
-  public static final String BUILD_DATE = "2023-12-30 20:04:01 EST";
-  public static final long BUILD_UNIX_TIME = 1703984641302L;
+  public static final String BUILD_DATE = "2024-01-04 19:01:03 EST";
+  public static final long BUILD_UNIX_TIME = 1704412863662L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
